598a2fd1479c5cdb7f0f494a600efae9fedd17aa
Clarence-6109 <chinemeremmadu.prsnl@gmail.com>
2025-12-29 10:02:24 +0100

    Update api.js

--- apps/frontend/analytics-dashboard/src/api/api.js
+++ apps/frontend/analytics-dashboard/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // your backend URL
+  baseURL: "https://analytics-dashboard-clarence.onrender.com", // your backend URL
 });
 
 // Automatically attach token from localStorage
